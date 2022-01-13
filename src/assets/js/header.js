3a37f732cc8cf549a99d2074f0c00f40d8c3c473
Raigon Otaku <41849189+raigon-otaku@users.noreply.github.com>
2022-01-13 08:24:16 +0000

    Initial commit

---
+++ src/assets/js/header.js
@@ -0,0 +1,7 @@
+function openSearch() {
+  document.getElementById("searchOverlay").style.display = "block";
+}
+
+function closeSearch() {
+  document.getElementById("searchOverlay").style.display = "none";
+}
